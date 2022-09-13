685eaec62f96af00f5618b89a5bde6a3277430af
Goodness <babasolagoodness21@gmail.com>
2022-09-13 14:57:02 +0100

    I updated style.css file

---
+++ script.js
@@ -0,0 +1,9 @@
+class Team{
+    constructor(playerName,jerseyNumber,position){
+        playerName = playerName,
+        jerseyNumber = jerseyNumber,
+        position = position
+    }
+}
+
+var player1 Team ("")
